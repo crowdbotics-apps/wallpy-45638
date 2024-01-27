f0772984faeb8a509795d897ffa39d70cf1748c7
Crowdbotics <team@crowdbotics.com>
2024-01-27 09:37:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wallpy-45638",
+  url: "https://wallpy-45638.botics.co",
+  api: "https://wallpy-45638.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
